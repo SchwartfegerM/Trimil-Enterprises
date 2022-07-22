c22897067be33a10e0cf87584924f079982f1874
MatthewSchwartf <>
2022-07-22 23:32:11 +1200

    added comments on functions

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 #set enviroment variables:
 #set FLASK_APP app.py
 from os import urandom
-from flask import Flask, render_template, session, redirect, request, url_for, jsonify
+from flask import Flask, render_template, session, redirect, request, url_for, jsonify #imports nessessary functions from flask
 from flask_session import Session
-from database_funtions import get_database_connection
+from database_funtions import get_database_connection #import get _database_connection function from my database library
 
 app = Flask(__name__) #initialise the flask application
 #config to set up sessions for a secure login system
 app.config["SESSION_TYPE"] = 'filesystem'
 app.config["SESSION_PERMANENT"] = False
-app.secret_key = urandom(24)
+app.secret_key = urandom(24) #creates a new secret key for the session on launch
 Session(app)
 
 
@@ -22,6 +22,7 @@ def index():
   conn.close()
   return render_template("index.html", posts = posts)
 
+#routes for the front end of the blog page
 @app.route("/blog",methods=["GET","POST"])
 #route for the list of blog posts
 def blog():
@@ -38,11 +39,14 @@ def posts(id):
   conn.close()
   return render_template("blog-post.html",content = posts[id-1])
 
+#routes for the login page
 @app.route("/login",methods=["GET","POST"])
+#serves the login page
 def login():
     return render_template("login.html")
 
 @app.route("/login/verify",methods=["GET","POST"])
+#verifies the login details
 def verify():
   if request.method == "POST":
     try:
@@ -62,12 +66,12 @@ def verify():
     return redirect(url_for("index"))
 
 @app.route("/logout",methods=["GET","POST"])
+#logs the user out
 def logout():
   session.pop("username", None)
   return redirect("/")
 
-#routes and funtions for the reviews component
-
+#routes for the reviews component
 @app.route("/reviews")
 #servs the list of all reviews in the database
 def review():
@@ -96,8 +100,7 @@ def review_submitted():
   conn.close() #close connection to the databse
   return render_template("submittedReview.html") #render the review submitted template
 
-#routes and functions for the ADMIN side of the blog component
-
+#routes for the backend of the blog page
 @app.route("/blog/list", methods=["GET","POST"])
 #this is the list of posts visable to ADMIN accounts
 def blog_posts_list():
@@ -110,95 +113,124 @@ def blog_posts_list():
     return redirect(url_for("index"))
 
 @app.route('/blog/create')
+#serves the page page to create a new blog post
 def create_blog_post():
   if "username" in session:
     return render_template("createBlogPost.html")
   else: 
-    return redirect(url_for("index"),post_url="blogpost/submit")
+    return redirect(url_for("index"))
 
 @app.route('/blogpost/submit',methods=["POST"])
-#remove this when blog post handling is done
+#stores the contents of the blog post form from creating a new blog post
 def blog_post_submit():
-  post_name=request.form.get("post_name",None)
-  post_date=request.form.get("post_date",None)
-  post_summary=request.form.get("post_summary",None)
-  post_content=request.form.get("post_content",None)
-  conn = get_database_connection()
-  conn.execute("INSERT INTO BlogPosts(Title,Date,Summary,Content) VALUES (?,?,?,?)",(post_name,post_date,post_summary,post_content))
-  conn.commit()
-  conn.close()
-  return redirect(url_for("blog_posts_list"),code="307")
+  if "username" in session:
+    post_name=request.form.get("post_name",None)
+    post_date=request.form.get("post_date",None)
+    post_summary=request.form.get("post_summary",None)
+    post_content=request.form.get("post_content",None)
+    conn = get_database_connection()
+    conn.execute("INSERT INTO BlogPosts(Title,Date,Summary,Content) VALUES (?,?,?,?)",(post_name,post_date,post_summary,post_content))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("blog_posts_list"),code="307")
+  else:
+    return redirect(url_for("index"))
 
 @app.route("/blog/<int:id>/edit")
+#serves the page to edit a blog post and inputs the data from the blog post into the form
 def blog_post_edit(id):
-  conn = get_database_connection()
-  post_data = conn.execute("SELECT * FROM BlogPosts WHERE id=?",(id,)).fetchall()
-  conn.close()
-  return render_template("createBlogPost.html", post=post_data, post_url="/blog/{}/edit/update".format(id))
+  if "username" in session:
+    conn = get_database_connection()
+    post_data = conn.execute("SELECT * FROM BlogPosts WHERE id=?",(id,)).fetchall()
+    conn.close()
+    return render_template("createBlogPost.html", post=post_data, post_url="/blog/{}/edit/update".format(id))
+  else:
+    return redirect(url_for("index"))
 
 @app.route("/blog/<int:id>/edit/update",methods=["POST"])
 def update_post(id):
-  post_name=request.form.get("post_name",None)
-  post_date=request.form.get("post_date",None)
-  post_summary=request.form.get("post_summary",None)
-  post_content=request.form.get("post_content",None)
-  conn=get_database_connection()
-  conn.execute("UPDATE BlogPosts SET title=?,date=?,Summary=?,content=? WHERE id=?",(post_name,post_date,post_summary,post_content,id))
-  conn.commit()
-  conn.close()
-  return redirect(url_for("blog_posts_list"),code=307)
+  if "username" in session:
+    post_name=request.form.get("post_name",None)
+    post_date=request.form.get("post_date",None)
+    post_summary=request.form.get("post_summary",None)
+    post_content=request.form.get("post_content",None)
+    conn=get_database_connection()
+    conn.execute("UPDATE BlogPosts SET title=?,date=?,Summary=?,content=? WHERE id=?",(post_name,post_date,post_summary,post_content,id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("blog_posts_list"),code=307)
+  else:
+    return redirect(url_for("index"))
 
 @app.route("/blog/<int:id>/delete")
+#deletes a blog post
 def blog_post_delete(id):
-  conn = get_database_connection()
-  conn.execute("DELETE FROM BlogPosts WHERE id=?",(id,))
-  conn.commit()
-  conn.close()
-  return redirect("/blog/list")
+  if "username" in session:
+    conn = get_database_connection()
+    conn.execute("DELETE FROM BlogPosts WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+    return redirect("/blog/list")
+  else:
+    return redirect(url_for("index"))
 
 #routes and functions for the invoicing system component
-
 @app.route("/invoicing")
+#serves the page to create a new invoice and view all invoices
 def invoicing():
-  return render_template("invoicing.html")
+  if "username" in session:
+    return render_template("invoicing.html")
+  else:
+    return redirect(url_for("index"))
 
 @app.route("/json")
+#serves the json file for the invoicing system
 def json():
-  conn = get_database_connection()
-  invoices = conn.execute("SELECT * FROM invoices").fetchall()
-  conn.close()
-  invoice = [dict(row) for row in invoices]
-  return jsonify(invoice)
+  if "username" in session:
+    conn = get_database_connection()
+    invoices = conn.execute("SELECT * FROM invoices").fetchall()
+    conn.close()
+    invoice = [dict(row) for row in invoices]
+    return jsonify(invoice)
+  else:
+    return redirect(url_for("index"))
 
 @app.route("/add/<string:action>/<int:id>/<string:part>/<string:new_value>", methods=["POST"])
-def add_invoice(action,id,part,new_value):
-  conn = get_database_connection()
-  if action == "UPDATE":
-    conn.execute("UPDATE invoices SET "+part+"=? WHERE id=?",(new_value,id))
-  elif action == "DELETE":
-    conn.execute("DELETE FROM invoices WHERE id=?",(id,))
-  else:
-    return "Not a valid action"
-  conn.commit()
-  conn.close()
-  return redirect(url_for("invoicing"))
-
-@app.route("/invoices/create", methods=["POST"])
-def create_invoice():
-  if request.method == "POST":
-    first_name = request.form["first_name"]
-    last_name = request.form["last_name"]
-    office_charges = request.form["office_charges"]
-    project_management = request.form["project_management"]
-    travel_expenses = request.form["travel_charges"]
-    consulation_fee = request.form["consultancy_charges"]
+#updates the database with the new values for the invoices or deletes the invoice
+def update_invoice(action,id,part,new_value):
+  if "username" in session:
     conn = get_database_connection()
-    conn.execute("INSERT INTO invoices(first_name,last_name,office_charges,project_management,travel_charges,consultancy_charges) VALUES (?,?,?,?,?,?)",(first_name,last_name,office_charges,project_management,travel_expenses,consulation_fee))
+    if action == "UPDATE":
+      conn.execute("UPDATE invoices SET "+part+"=? WHERE id=?",(new_value,id))
+    elif action == "DELETE":
+      conn.execute("DELETE FROM invoices WHERE id=?",(id,))
+    else:
+      return "Not a valid action"
     conn.commit()
     conn.close()
     return redirect(url_for("invoicing"))
-    #could put in a total here if time presents itself
+  else:
+    return redirect(url_for("index"))
+
+@app.route("/invoices/create", methods=["POST"])
+#creates a new invoice
+def create_invoice():
+  if request.method == "POST":
+    if "username" in session:
+      first_name = request.form["first_name"]
+      last_name = request.form["last_name"]
+      office_charges = request.form["office_charges"]
+      project_management = request.form["project_management"]
+      travel_expenses = request.form["travel_charges"]
+      consulation_fee = request.form["consultancy_charges"]
+      conn = get_database_connection()
+      conn.execute("INSERT INTO invoices(first_name,last_name,office_charges,project_management,travel_charges,consultancy_charges) VALUES (?,?,?,?,?,?)",(first_name,last_name,office_charges,project_management,travel_expenses,consulation_fee))
+      conn.commit()
+      conn.close()
+      return redirect(url_for("invoicing"))
+    else:
+      return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=81,debug=True)
+  app.run(host='0.0.0.0', port=81,debug=True) #runs the app on port 81
